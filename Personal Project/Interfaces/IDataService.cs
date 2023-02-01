5a4fa1629ef6d74d7fd9aa82c9dc0bce7c179571
Matthias McCarthy <matthias.mccarthy@gmail.com>
2023-02-01 21:24:53 +0100

    Add project files.

---
+++ Personal Project/Interfaces/IDataService.cs
@@ -0,0 +1,10 @@
+﻿using Personal_Project.Models;
+
+namespace Personal_Project.Interfaces
+{
+    public interface IDataService
+    {
+        Task Create(Post model);
+        Task<List<Post>> GetAll();
+    }
+}
---
+++ Personal Project/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace Personal_Project.Models
+{
+    [DynamoDBTable(tableName:"post")]
+    public class Post
+    {
+        public Post()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+        public DateTimeOffset PostDateTime { get; set; }
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
